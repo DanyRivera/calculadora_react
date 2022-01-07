0785c2114ebe6a361ce7bc9c3b00fac47725695c
Dany <dany@gmail.com>
2022-01-07 14:51:54 -0600

    v1.1

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -3,7 +3,7 @@ import Resultado from "./Resultado";
 import BotonesCalculadora from "./BotonesCalculadora";
 
 const ContenedorCalculadora = styled.div`
-  width: 70%;
+  width: 80%;
   height: 50%;
   padding: 20px 20px 135px 20px;
   border-radius: 5px;
@@ -11,12 +11,16 @@ const ContenedorCalculadora = styled.div`
   backdrop-filter: blur( 20px );
   box-shadow: 0 2.5px 18px #0000005e;
 
-  @media (min-width: 876px) {
+  @media (min-width: 480px) {
     width: 40%;
   }
 
+  @media (min-width: 876px) {
+    width: 30%;
+  }
+
   @media (min-width: 1230px) {
-    width: 25%;
+    width: 23%;
   }
 
 `;
